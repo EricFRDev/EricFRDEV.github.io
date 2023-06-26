121fbf1ec280ae4f19d60a5a4022dcde034c13e1
EricFRDev <31632656+EricFRDev@users.noreply.github.com>
2023-06-26 17:00:27 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -21,19 +21,19 @@ for (var i=0; themeDots.length > i; i++){
 
 function setTheme(mode){
     if(mode == 'light'){
-        document.getElementById('theme-style').href = 'default.css'
+        document.getElementById('theme-style').href = '/stylesheets/default.css'
     }
 
     if(mode == 'blue'){
-        document.getElementById('theme-style').href = 'blue.css'
+        document.getElementById('theme-style').href = '/stylesheets/blue.css'
     }
 
     if(mode == 'green'){
-        document.getElementById('theme-style').href = 'green.css'
+        document.getElementById('theme-style').href = '/stylesheets/green.css'
     }
 
     if(mode == 'purple'){
-        document.getElementById('theme-style').href = 'purple.css'
+        document.getElementById('theme-style').href = '/stylesheets/purple.css'
     }
 
     localStorage.setItem('theme', mode)
